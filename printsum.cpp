cd6cb6f11e396230b6b82fcf3ebe3f07e53d141d
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-07-14 11:12:49 +0530

    print sum of two numbers

---
+++ printsum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cout << "enter a : " << endl;
+    cin >> a;
+
+    cout << "enter b : " << endl;
+    cin >> b;
+
+    int sum = a + b;
+    cout << "sum of a and b is : " << (sum) << endl;
+    return 0;
+
+}
