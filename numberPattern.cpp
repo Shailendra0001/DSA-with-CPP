68d68be77eb69358e34cc1fa54dfafbde8e37c0d
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-07-18 21:18:21 +0530

    print number

---
+++ numberPattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout << "enter no. of rows : ";
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int count = 1;
+        for(int j=0; j<=i; j++){
+            cout << count << " ";
+            count ++;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
