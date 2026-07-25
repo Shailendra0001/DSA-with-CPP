93d7687a36b9830931c4ec49d5ed994962e63b53
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-07-25 20:56:16 +0530

    add sum of number using functions

---
+++ printSumm.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a , int b){
+
+    int sum = a+b;
+    return sum;
+
+}
+
+int main() {
+
+    cout << sum(2,5) << endl;;
+    
+    return 0;
+}
