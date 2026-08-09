b9f89e3886cf879e0518420b4796721606b37ef5
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-08-09 21:08:37 +0530

    Added array traversal using pointer

---
+++ pointerArray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    int *ptr = arr;
+
+    for(int i = 0; i < 5; i++) {
+        cout << *ptr << " ";
+        ptr++;
+    }
+
+    return 0;
+}
